20812ba6aa8c75f8efc87e1cf27b6f6dbff210b0
Christian <christian.sy@hotmail.com>
2018-04-08 21:47:11 -0700

    linked settings button to corresponding layout page

---
+++ app/src/main/java/com/example/christian/acnoride/MainMenu.java
@@ -0,0 +1,22 @@
+package com.example.christian.acnoride;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainMenu extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_menu);
+
+    }
+
+    public void settings(View view){
+        Intent startNewActivity = new Intent(this, Settings.class);
+        startActivity(startNewActivity);
+        this.overridePendingTransition(0, 0);
+    }
+}
